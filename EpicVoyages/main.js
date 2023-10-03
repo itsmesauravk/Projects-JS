a72a1b88de076213dc6335a3b9e2c139424dd6e8
itsmesauravk <sauravkarki10.12@gmail.com>
2023-10-03 23:07:45 +0545

    body banner updated

--- EpicVoyages/main.js
+++ EpicVoyages/main.js
@@ -4,6 +4,7 @@ const searchBox = document.querySelector(".search-box")
 const menuIcon = document.getElementById("menu-icon")
 const menuCloseIcon = document.getElementById("menu-close-icon")
 const navOptionsAfter = document.querySelector(".nav-options-after")
+const header = document.getElementById("header")
 
 searchIcon.addEventListener("click", () => {
   searchIcon.style.display = "none"
@@ -25,3 +26,54 @@ menuCloseIcon.addEventListener("click", () => {
   menuIcon.style.display = "flex"
   menuCloseIcon.style.display = "none"
 })
+
+// this will change the background of the navbar when scrolled
+function updateNavbarBackground() {
+  if (window.scrollY > 600) {
+    header.classList.add("scrolled")
+  } else {
+    header.classList.remove("scrolled")
+  }
+}
+window.addEventListener("scroll", updateNavbarBackground)
+
+// for swiperJS
+const swiperWrapper = document.querySelector(".swiper-wrapper")
+const swiperSlide = document.querySelector(".swiper-slide")
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
+
+const gettingData = () => {
+  fetch("./banner.json")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      data.banners.forEach(item => {
+        const slide = document.createElement("div")
+        slide.classList.add("swiper-slide")
+        slide.style.backgroundImage = `url('${item.image}')`
+
+        swiperWrapper.appendChild(slide)
+        slide.innerHTML = `
+        <div id='data-container'>
+          <div id='heading'>${item.details.heading}</div>
+          <div id='subheading'>${item.details.subheading} </div> 
+          <button id='banner-btn'>Get Started </button>
+        </div>
+        `
+      })
+      swiper.update()
+    })
+}
+
+gettingData()
