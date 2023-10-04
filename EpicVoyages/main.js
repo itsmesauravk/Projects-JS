3364fd5fdea1e6ffa19c20305a610a3ac60ac4b6
itsmesauravk <sauravkarki10.12@gmail.com>
2023-10-04 19:08:00 +0545

    body updated

--- EpicVoyages/main.js
+++ EpicVoyages/main.js
@@ -54,7 +54,7 @@ const swiper = new Swiper(".swiper", {
 })
 
 const gettingData = () => {
-  fetch("./banner.json")
+  fetch("./json/banner.json")
     .then(res => res.json())
     .then(data => {
       console.log(data)
@@ -75,5 +75,99 @@ const gettingData = () => {
       swiper.update()
     })
 }
-
 gettingData()
+
+//adventure idea -----------------------------------------------------------------------------
+const adventureContainer = document.querySelector(".a-container")
+
+const adventureIdea = async () => {
+  try {
+    const response = await fetch("./json/adventure.json")
+    if (!response.ok) {
+      throw new Error("Network is not responding correctly")
+    }
+    const ideas = await response.json()
+    const adventureOptions = ideas.adventureIdeas
+    adventureCard(adventureOptions)
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error)
+  }
+}
+
+adventureIdea()
+
+function adventureCard(adventureOptions) {
+  adventureContainer.innerHTML = ""
+  for (let i = 0; i < 4; i++) {
+    let option = adventureOptions[i]
+
+    adventureContainer.innerHTML += `
+    <div class="a-card">
+      <div class="a-image">
+        <img
+          src="${option.image}"
+          alt=" image"
+        />
+      </div>
+      <div class="a-details">
+        <div class="a-details-name">${option.name}</div>
+        <div class="a-details-short">
+         <p>
+         ${option.details}
+          </p>
+        </div>
+      </div>
+    <button id='a-read-more' class='a-read-more'>Read More</button>
+  </div>`
+  }
+}
+
+// featured products js -------------------------------------------------------------------------
+const container = document.querySelector(".f-container")
+
+const featuredItems = async () => {
+  try {
+    const response = await fetch("./json/featuredProduct.json")
+    if (!response.ok) {
+      throw new Error("Network is not responding correctly")
+    }
+    const products = await response.json()
+    const featuredProducts = products.featuredProducts
+    showDataInCard(featuredProducts)
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error)
+  }
+}
+
+featuredItems()
+
+// show data in card
+function showDataInCard(featuredProducts) {
+  container.innerHTML = ""
+  for (let i = 0; i < 4; i++) {
+    let product = featuredProducts[i]
+
+    container.innerHTML += `
+        <div class="f-card">
+          <div class="f-card-image">
+            <img src="${product.image}" alt="image" />
+          </div>
+          <div class="f-card-details">
+            <p id="card-name">${product.name}</p>
+            <p id="card-price">$${product.price}</p>
+            <p id="card-rating">${product.rating}</p>
+          </div>
+          <div class="f-card-btns">
+            <button type="submit" class="btn add-to-cart">
+              <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+            <button type="submit" class="btn watch-card">
+              <i class="fa-solid fa-eye"></i>
+            </button>
+            <button type="submit" class="btn share-card">
+              <i class="fa-solid fa-share"></i>
+            </button>
+          </div>
+        </div>`
+  }
+}
