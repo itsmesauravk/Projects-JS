b9d58568293f856058cad069a2a12455c7d80270
itsmesauravk <sauravkarki10.12@gmail.com>
2023-10-31 23:06:10 +0545

    updated portfolio home

--- Portfolio/portfolio1/src/App.js
+++ Portfolio/portfolio1/src/App.js
@@ -1,12 +1,35 @@
 import "./App.css"
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
+import { useState } from "react"
 
 function App() {
+  const [mode, setMode] = useState("light")
+
+  const removeBodyClasses = () => {
+    document.body.classList.remove("bg-success")
+    document.body.classList.remove("bg-info")
+    document.body.classList.remove("bg-danger")
+    document.body.classList.remove("bg-secondary")
+    document.body.classList.remove("bg-primary")
+  }
+  const changeThemeMode = currentMode => {
+    removeBodyClasses()
+    document.body.classList.add(`bg-${currentMode}}`)
+    if (mode === "light") {
+      setMode("dark")
+      document.body.style.backgroundColor = "#393E46"
+      document.body.style.color = "#fff"
+    } else {
+      setMode("light")
+      document.body.style.backgroundColor = "#fff"
+      document.body.style.color = "#222831"
+    }
+  }
   return (
     <>
       <header>
-        <Navbar />
+        <Navbar mode={mode} changeThemeMode={changeThemeMode} />
         <Home />
       </header>
     </>
--- Portfolio/portfolio1/src/components/Navbar.js
+++ Portfolio/portfolio1/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react"
+// import { Link } from "react-router-dom"
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <nav>
       <div className="logo">
@@ -9,31 +10,28 @@ export default function Navbar() {
         </p>
       </div>
       <div className="navs">
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-          <div className="form-check form-switch">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-            />
-            <label className="form-check-label" for="flexSwitchCheckDefault">
-              Dark
-            </label>
-          </div>
-        </ul>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+        <div
+          className={`form-check form-switch text-${
+            props.mode === "light" ? "dark" : "light"
+          }`}
+        >
+          <input
+            className="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="flexSwitchCheckDefault"
+            onClick={() => {
+              props.changeThemeMode(null)
+            }}
+          />
+          <label className="form-check-label" for="flexSwitchCheckDefault">
+            {props.mode === "light" ? "Dark" : "Light"}
+          </label>
+        </div>
       </div>
     </nav>
   )
