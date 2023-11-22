2b5444f1717f78c17cd6b8d7a027db8fc728c736
itsmesauravk <sauravkarki10.12@gmail.com>
2023-11-22 20:48:14 +0545

    completed skills update

--- Portfolio/portfolio1/src/components/Skills.js
+++ Portfolio/portfolio1/src/components/Skills.js
@@ -39,6 +39,26 @@ export default function Skills() {
           src="https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png"
           alt="expressjs"
         />
+        <img
+          className="skills-img"
+          src="https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png"
+          alt="sql"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png"
+          alt="python"
+          className="skills-img"
+        />
+        <img
+          src="https://static-00.iconduck.com/assets.00/tailwind-css-icon-2048x1229-u8dzt4uh.png"
+          alt="tailwing"
+          className="skills-img"
+        />
+        <img
+          src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/bootstrap-5-logo-icon.png"
+          alt="bootstrap"
+          className="skills-img"
+        />
       </div>
     </div>
   )
