93a21383de7f4fef90734c8d3920f1e752661e11
itsmesauravk <sauravkarki10.12@gmail.com>
2023-11-27 07:28:45 +0545

    continue mern-bloh

--- FullStack_Blog_writing/client/src/App.js
+++ FullStack_Blog_writing/client/src/App.js
@@ -6,19 +6,19 @@ import Layout from "./Layout"
 import { Route, Routes } from "react-router-dom"
 import LoginPage from "./Pages/LoginPage"
 import RegisterPage from "./Pages/RegisterPage"
-// import { UserContextProvider } from "./UserContext"
+import { UserContextProvider } from "./UserContext"
 
 function App() {
   return (
-    // <UserContextProvider>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
-    // </UserContextProvider>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   )
 }
 
