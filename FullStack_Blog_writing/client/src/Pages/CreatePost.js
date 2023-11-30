5504773ac462dd75b93df250b486822cfd7c5e69
itsmesauravk <sauravkarki10.12@gmail.com>
2023-12-01 00:27:09 +0545

    continue mern-blog

--- FullStack_Blog_writing/client/src/Pages/CreatePost.js
+++ FullStack_Blog_writing/client/src/Pages/CreatePost.js
@@ -33,30 +33,62 @@ const formats = [
 export default function CreatePost() {
   const [title, setTitle] = useState("")
   const [summary, setSummary] = useState("")
+  const [files, setFiles] = useState("")
   const [content, setContent] = useState("")
 
+  async function createPost(ev) {
+    ev.preventDefault()
+
+    const data = new FormData()
+    data.set("title", title)
+    data.set("summary", summary)
+    data.set("files", files[0])
+    data.set("content", content)
+
+    try {
+      const response = await fetch("http://localhost:4000/post", {
+        method: "POST",
+        body: data,
+      })
+
+      if (!response.ok) {
+        throw new Error("Failed to create post")
+      }
+
+      // Reset form fields after successful post creation
+      setTitle("")
+      setSummary("")
+      setFiles("")
+      setContent("")
+    } catch (error) {
+      console.error("Error creating post:", error.message)
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={createPost}>
       <input
-        type="title"
+        type="text"
         placeholder="title"
         value={title}
         onChange={ev => setTitle(ev.target.value)}
       />
       <input
-        type="summary"
+        type="text"
         placeholder="summary"
         value={summary}
         onChange={ev => setSummary(ev.target.value)}
       />
-      <input type="file" />
+      <input type="file" onChange={ev => setFiles(ev.target.files)} />
       <ReactQuill
         value={content}
         modules={modules}
         formats={formats}
         onChange={newValue => setContent(newValue)}
       />
-      <button style={{ marginTop: "2rem" }}>Create Post</button>
+      <button style={{ marginTop: "2rem" }} type="submit">
+        Create Post
+      </button>
     </form>
   )
 }
