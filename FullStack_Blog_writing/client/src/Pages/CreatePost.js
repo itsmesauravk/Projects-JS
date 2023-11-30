466e1912ab267afb1e352d588195184d2bb9630a
itsmesauravk <sauravkarki10.12@gmail.com>
2023-11-30 23:53:56 +0545

    continue mern-blog

--- FullStack_Blog_writing/client/src/Pages/CreatePost.js
+++ FullStack_Blog_writing/client/src/Pages/CreatePost.js
@@ -1,12 +1,44 @@
+import { useState } from "react"
 import ReactQuill from "react-quill"
 import "react-quill/dist/quill.snow.css"
 export default function CreatePost() {
+  const [title, setTitle] = useState("")
+  const [summary, setSummary] = useState("")
+  const [content, setContent] = useState("")
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  }
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ]
+
   return (
     <form>
       <input type="title" placeholder="title" />
       <input type="summary" placeholder="summary" />
       <input type="file" />
-      <ReactQuill />
+      <ReactQuill value={content} modules={modules} formats={formats} />
       <button style={{ marginTop: "2rem" }}>Create Post</button>
     </form>
   )
