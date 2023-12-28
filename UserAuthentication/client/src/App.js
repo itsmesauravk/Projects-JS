862892b34ab47cc98ebb78d90e0b5b8f577fea43
itsmesauravk <sauravkarki10.12@gmail.com>
2023-12-29 00:19:12 +0545

    page routing continue

--- UserAuthentication/client/src/App.js
+++ UserAuthentication/client/src/App.js
@@ -3,14 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<HomePage/>} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
