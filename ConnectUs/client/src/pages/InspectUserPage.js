f52953b75632a159d948ae42cac7a4c72bbf511b
itsmesauravk <sauravkarki10.12@gmail.com>
2024-01-16 01:52:04 +0545

    minor update

--- ConnectUs/client/src/pages/InspectUserPage.js
+++ ConnectUs/client/src/pages/InspectUserPage.js
@@ -38,9 +38,9 @@ export default function InspectUserPage({mode}) {
 
 
     const imageLink = "http://localhost:4000/"
-    // const blueTick = "https://upload.wikimedia.org/wikipedia/commons/3/32/Verified-badge.png";
-    // const goldTick = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Twitter_Verified_Badge_Gold.svg/2048px-Twitter_Verified_Badge_Gold.svg.png";
-    console.log("user: ", userData);
+    const blueTick = "https://upload.wikimedia.org/wikipedia/commons/3/32/Verified-badge.png";
+    const goldTick = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Twitter_Verified_Badge_Gold.svg/2048px-Twitter_Verified_Badge_Gold.svg.png";
+    // console.log("user: ", userData);
     console.log("userPosts: ", userPosts);
 
     if (redirect) {
@@ -55,34 +55,44 @@ export default function InspectUserPage({mode}) {
           >
             Back
           </button>
-          <h1 className="font-bold text-2xl mt-5">{userData?.firstName}'s Profile :</h1>
+            {!userData && <p className="font-bold text-2xl mt-10">User have no Post uploaded !!</p>}
+
+          {userData && (
+            <div>
+                <h1 className="font-bold text-2xl mt-5">{userData?.firstName}'s Profile :</h1>
     
-          <div>
-            {!userPosts && <p>User have no Post uploaded !!</p>}
-            {userPosts.map((post) => (
-              <div key={post._id} className={`flex flex-col gap-5 mt-3 mb-3 p-2.5 border-2 rounded-md `}>
-                <div class={`bg-white shadow-lg rounded-md p-4 mb-6 ${mode === 'light'?'light-theme':'dark-theme'}`}>
-                    <div class="flex items-center mb-4">
+                <div>
+                
+                {userPosts.map((post) => (
+                    <div key={post._id} className={`flex flex-col gap-5 mt-3 mb-3 p-2.5 border-2 rounded-md `}>
+                    <div class={`bg-white shadow-lg rounded-md p-4 mb-6 ${mode === 'light'?'light-theme':'dark-theme'}`}>
+                        <div class="flex items-center mb-4">
+                            <img
+                            class="w-16 h-16 rounded-full object-cover mr-4"
+                            src={imageLink + post.user.profileImage}
+                            alt="profile"
+                            />
+                            <div>
+                                <div className="flex gap-2">
+                                    <h1 class="text-xl font-bold">{post.user.firstName} {post.user.surname}</h1>
+                                    {post.user.vipToken === "iamgold" && <img src={goldTick} alt="gold" className="w-6 h-6" />}
+                                    {post.user.vipToken === "skyisblue" && <img src={blueTick} alt="blue" className="w-6 h-6" />}
+                                </div>
+                                <p class="text-gray-600">{post.user.email}</p>
+                            </div>
+                        </div>
+                        <p class="text-lg mb-4">{post.caption}</p>
                         <img
-                        class="w-16 h-16 rounded-full object-cover mr-4"
-                        src={imageLink + post.user.profileImage}
-                        alt="profile"
+                            class="rounded-md w-full h-150"
+                            src={imageLink + post.image}
+                            alt="post image"
                         />
-                        <div>
-                        <h1 class="text-xl font-bold">{post.user.firstName} {post.user.surname}</h1>
-                        <p class="text-gray-600">{post.user.email}</p>
                         </div>
                     </div>
-                    <p class="text-lg mb-4">{post.caption}</p>
-                    <img
-                        class="rounded-md w-full h-150"
-                        src={imageLink + post.image}
-                        alt="post image"
-                    />
-                    </div>
-              </div>
-            ))}
-          </div>
+                ))}
+                </div>
+            </div>
+            )}
         </div>
       );
     
